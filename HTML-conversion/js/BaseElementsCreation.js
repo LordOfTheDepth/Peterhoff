5d36ba0088e757c16e35c0fe78d59306fe534ec2
LordOfTheDepth <77456883+LordOfTheDepth@users.noreply.github.com>
2025-12-24 07:24:40 +0300

    123

--- HTML-conversion/js/BaseElementsCreation.js
+++ HTML-conversion/js/BaseElementsCreation.js
@@ -1,5 +1,5 @@
 
-function createBaseElements(headerTitle, headerImageUrl)
+function createBaseElements(headerTitle, headerImageUrl, button1, button2, button3)
 {
     
   createElement("background","https://lordofthedepth.github.io/Peterhoff/HTML-conversion/html/Background.html")
@@ -9,6 +9,9 @@ function createBaseElements(headerTitle, headerImageUrl)
     insertHtml("page-title-img", `<img src = ${headerImageUrl}> </img>`)
 });
   createElement("buttons","https://lordofthedepth.github.io/Peterhoff/HTML-conversion/html/PeterhofButtons.html?refresh=1")
+  .then(() => {insertHtml("button1",`<a href="https://spbarchives.ru/${button1}">До войны</a>`)})
+  .then(() => {insertHtml("button2",`<a href="https://spbarchives.ru/${button2}">Разрушение</a>`)})
+  .then(() => {insertHtml("button3",`<a href="https://spbarchives.ru/${button3}">Восстановление</a>`)})
 
   createElement("footer-container","https://lordofthedepth.github.io/Peterhoff/HTML-conversion/html/Footer.html?refresh=1")
 
