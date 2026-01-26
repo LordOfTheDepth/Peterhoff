cdf382d18a0eadb3efb70565917a71ad439d6f75
LordOfTheDepth <Ldk99@yandex.ru>
2026-01-26 13:16:47 +0300

    123

--- HTML-conversion/js/BaseElementsCreation.js
+++ HTML-conversion/js/BaseElementsCreation.js
@@ -2,41 +2,47 @@
 function createBaseElements(headerTitle, headerImageUrl, button1, button2, button3, locationId, stageId)
 {
     
+beforeCode = "%D0%B4%D0%BE%20%D0%B2%D0%BE%D0%B9%D0%BD%D1%8B"
+destructionCode = "%D1%80%D0%B0%D0%B7%D1%80%D1%83%D1%88%D0%B5%D0%BD%D0%B8%D1%8F"
+restorationCode = "%D0%B2%D0%BE%D1%81%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5"
+stagesCodes = [beforeCode,destructionCode,restorationCode]
+stages = ["До войны", "Разрушения", "Восстановление"]
+locations = ["Петергоф","Пушкин","Павловск"]
+headerImages = [[
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_Ptrg.jpg?raw=true",
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_Ptrg_war.jpg?raw=true",
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_Ptrg_vosst.jpg?raw=true",
+],[
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_push_dovoin.jpg?raw=true",
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_push_razrush.jpg?raw=true",
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_push_vosst.jpg?raw=true",
+],[
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_pavl_01.jpg?raw=true",
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_pavl_03.jpg?raw=true",
+"https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/verh_pavl_02.jpg?raw=true",
+]]
+
+
+headerTitle = `${locations[locationId-1]} : ${stages[stageId-1]}`
+
 createElement("background","https://lordofthedepth.github.io/Peterhoff/HTML-conversion/html/Background.html")
 
 createElement("header","https://lordofthedepth.github.io/Peterhoff/HTML-conversion/html/PeterhofHeader.html").then(() => {insertHtml("page-title",headerTitle)}).then(() => 
 {
-insertHtml("page-title-img", `<img src = ${headerImageUrl}> </img>`) 
-logoUrl = ""
-if(locationId == 1)
-{
-    logoUrl ="https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/Log_PetergofAsset%201@4x.png?raw=true"
-}
-if(locationId == 2)
-{
-    logoUrl = "https://github.com/LordOfTheDepth/Peterhoff/blob/main/Misc/%D0%A6%D0%B0%D1%80%D1%81%D0%BA%D0%BE%D0%B5%20%D1%81%D0%B5%D0%BB%D0%BE.png?raw=true"
-}
-if(logoUrl != "")
-{
-  //insertHtml("location-logo", `<img src = ${logoUrl}> </img>`)
-}
+insertHtml("page-title-img", `<img src = ${headerImages[locationId-1][stageId-1]}> </img>`) 
   
 });
 createElement("buttons","https://lordofthedepth.github.io/Peterhoff/HTML-conversion/html/PeterhofButtons.html?refresh=1")
 .then(() => {insertHtml("button1",`<a href="https://spbarchives.ru/${button1}">До войны</a>`)})
-.then(() => {insertHtml("button2",`<a href="https://spbarchives.ru/${button2}">Разрушение</a>`)})
+.then(() => {insertHtml("button2",`<a href="https://spbarchives.ru/${button2}">Разрушения</a>`)})
 .then(() => {insertHtml("button3",`<a href="https://spbarchives.ru/${button3}">Восстановление</a>`)})
 
 createElement("footer-container","https://lordofthedepth.github.io/Peterhoff/HTML-conversion/html/Footer.html?refresh=1")
 
 
-beforeCode = "%D0%B4%D0%BE%20%D0%B2%D0%BE%D0%B9%D0%BD%D1%8B"
-destructionCode = "%D1%80%D0%B0%D0%B7%D1%80%D1%83%D1%88%D0%B5%D0%BD%D0%B8%D1%8F"
-restorationCode = "%D0%B2%D0%BE%D1%81%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5"
-stages = [beforeCode,destructionCode,restorationCode]
-locations = ["Петергоф","Пушкин","Павловск"]
 
-const initialUrl = `https://lordofthedepth.github.io/Peterhoff/SortedMap/${locations[locationId-1]}/${stages[stageId-1]}`;
+
+const initialUrl = `https://lordofthedepth.github.io/Peterhoff/SortedMap/${locations[locationId-1]}/${stagesCodes[stageId-1]}`;
 textURL = initialUrl + "/text.html"
 
 createElement("main-text-container", textURL)
