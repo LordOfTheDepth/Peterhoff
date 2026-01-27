ad491744f590ab6690e8436b84850faf08a43f3b
LordOfTheDepth <Ldk99@yandex.ru>
2026-01-27 16:48:53 +0300

    123

--- Categorizer-map.py
+++ Categorizer-map.py
@@ -90,7 +90,7 @@ def DoFolder(source_folder, dest_folder):
     dest_folder_fixed = CategorizerUtills.resolve_relative_path(dest_folder)
 
     thumbnail_generator = ThumbnailGenerator(
-        max_size=(200, 200),
+        max_size=(300, 300),
         quality=100,
         format='JPEG'
     )
